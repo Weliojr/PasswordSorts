df131796ea62a8ee0f8c8b559a7aa270d65ca739
Weliojr <welio.juniorw@gmail.com>
2022-10-26 00:32:07 -0300

    atualização 25/10

---
+++ CriarCsv.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+public class CriarCsv {
+    public static void main(String[] args) throws IOException {
+        String path = "teste.csv";
+        String array[][] = {{"welio"},{"é"},{"moral"},{"demais"},{"nessa"},{"porra"}};
+        File product = new File(path);
+        FileWriter escritor = new FileWriter(product);
+        BufferedWriter bescritor = new BufferedWriter(escritor);
+        System.out.println(array.length);
+        for(int i=0; i<array.length;i++){
+            bescritor.write(array[i][0]);
+            bescritor.write(",");
+            bescritor.newLine();
+        }
+        bescritor.close();
+        escritor.close();
+    }
+    /*public static void Escrever(String path, String[] linhas) throws IOException{
+        // Inicializa o objeto 'arquivo' como caminho do arquivo desejado
+        File arquivo = new File(path);
+        // Inicializa o objeto 'fw' para inicializar o contrutor 'bw'
+        FileWriter fw = new FileWriter(arquivo);
+
+        // Construtor recebe como argumento o objeto do tipo FileWriter
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < linhas.length; i++){
+            //Escreve o conteúdo no arquivo
+            bw.write(linhas[i]);
+            // Adiciona a quebra de linha no arquivo
+            bw.newLine();
+            
+        }
+        // Fecha os recursos
+        bw.close();
+        fw.close();
+    }
+    public static void FiltrarCSV(String[][] MatrizString, String archiveName, int[] listaPosicao, int tamanho) throws IOException{
+        // Inicializa variavel 'linhas' para formar as linhas do arquivo
+        String[] linhas = new String[tamanho];
+
+        for (int i = 0; i < tamanho; i++){
+            linhas[i] = ""; // Inicia cada linha com vazio
+            for (int j = 0; j < 16; j++){
+                // Adiciona a primeira linha do arquivo os nomes das colunas
+                if (i == 0) linhas[i] += MatrizString[i][j];
+                else {
+                    if (j < 15) { // Verifica se é ou não o último elemento
+                        // Concatena cada variavel com uma virgula as separando
+                        linhas[i] += MatrizString[listaPosicao[i]][j] + ","; 
+                    }
+                    else linhas[i] += MatrizString[listaPosicao[i]][j]; // Concatena o último elemento da linha 
+                }
+            }
+        }
+        // Chama a função para criar o arquivo especificado e caso exista o apaga
+       // String transformar = VerificarExistencia(archiveName);
+        // Chama a função para escrever no arquivo especificado o que está contido na variavel 'linhas'
+        //Escrever(transformar, linhas);
+    }*/
+}
+
--- LerCsv.java
+++ LerCsv.java
@@ -2,12 +2,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+//import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+
 import java.security.cert.CertPath;
 import java.util.Arrays;
 public class LerCsv {
     
     public static void main(String[] args) {
+        int[] indice = new int [698878];
+        String senha[] = new String [698878];
+        int tamanhosenha[] = new int[698878];
+        String data[] = new String[698878];
         String arquivocsv = "/home/welio/Downloads/passwords.csv";
+        String path = "teste.csv";
         String[][] superarray = new String[698878][4];
         BufferedReader conteudocsv= null;
         int contador=0;
@@ -46,22 +57,82 @@ public class LerCsv {
             }
             
         }
-        //for(int i=0;i<698878;i++){
-            //for(int j=0;j<4;j++){
-                //System.out.print(superarray[i][j]+" ");
-           // }
-           // System.out.print("\n");
-       // }
        int contador2 =0;
+      //mergeSort(superarray);
        while(superarray[contador2][0] != null){
-            System.out.print(superarray[contador2][0]+" "+superarray[contador2][1]+" "+superarray[contador2][2]+" "+superarray[contador2][3]+" ");
-            System.out.print("\n");
+            //indice[contador2] = Integer.parseInt(superarray[contador2][0]);
+            senha[contador2] = superarray[contador2][1];
+            //tamanhosenha[contador2] = Integer.parseInt(superarray[contador2][2]);
+            data[contador2] = superarray[contador2][3];
+            System.out.print(/*indice[contador2]+" "+*/senha[contador2]+" "+/*tamanhosenha[contador2]+" "+*/data[contador2]+" ");
+            //System.out.print("\n");
             contador2++;
         }
-        System.out.println("\n O valor do contador2 é: "+contador2);
-        //vamos la atualizar 
-        //assim que der envio
-        //conseguindo adaptar com o git
-        
+
     }
+    //ALGORITMO MERGE SORT
+    public static void mergeSort(String[][] vetor) {
+		//int arraycopiamerge[][] = vetor.clone();
+		int tamanhoarray = vetor.length;
+        int dividirarray = tamanhoarray / 2;
+        String arraydaesquerda[][] = new String[dividirarray][4];
+        String arraydadireita[][] = new String[tamanhoarray - dividirarray][4];
+        if (tamanhoarray < 2) {
+            return;
+        }
+        for (int i = 0; i < dividirarray; i++) {
+            arraydaesquerda[i][2] = vetor[i][2];
+        }
+        for (int j = dividirarray; j < tamanhoarray; j++) {
+            arraydadireita[j - dividirarray][2] = vetor[j][2];
+        }
+        mergeSort(arraydaesquerda);
+        mergeSort(arraydadireita);
+        juntandomerge(vetor, arraydaesquerda, arraydadireita);
+        
+	}
+	//algoritmo juntando o merge
+	public static void juntandomerge(String entrada[][], String arraydaesquerda[][], String arraydadireita[][]) {
+        int tamanhoarrayesquerda = arraydaesquerda.length;
+        int tamanhoarraydireita = arraydadireita.length;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (a < tamanhoarrayesquerda && b < tamanhoarraydireita) {
+            if (Integer.parseInt(arraydaesquerda[a][2]) <= Integer.parseInt(arraydadireita[b][2])) {
+                entrada[c][2] = arraydaesquerda[a][2];
+                a = a + 1;
+            } else {
+                entrada[c][2] = arraydadireita[b][2];
+                b = b + 1;
+            }
+            c = c + 1;
+        }
+        while (a < tamanhoarrayesquerda) {
+            entrada[c][2] = arraydaesquerda[a][2];
+            a = a + 1;
+            c = c + 1;
+        }
+        while (b < tamanhoarraydireita) {
+            entrada[c][2] = arraydadireita[b][2];
+            b = b + 1;
+            c = c + 1;
+        }
+
+    }
+    /*public void criarcsv(String  path, String[][] arra){
+        
+        String array[][] = {{"welio"},{"é"},{"moral"},{"demais"},{"nessa"},{"porra"}};
+        File product = new File(path);
+        FileWriter escritor = new FileWriter(product);
+        BufferedWriter bescritor = new BufferedWriter(escritor);
+        System.out.println(array.length);
+        for(int i=0; i<array.length;i++){
+            bescritor.write(array[i][0]);
+            bescritor.write(",");
+            bescritor.newLine();
+        }
+        bescritor.close();
+        escritor.close();
+    }*/
 }
